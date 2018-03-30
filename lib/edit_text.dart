7482feb84dedbdcbc40b253e426501f76fa5a591
Craig Bilner <craig.bilner@news.co.uk>
2018-03-30 06:25:25 +0100

    fix: make editable text stateless

--- lib/edit_text.dart
+++ lib/edit_text.dart
@@ -1,44 +1,55 @@
 import 'package:flutter/material.dart';
 
-class EditTextPage extends StatelessWidget {
+class EditTextPage extends StatefulWidget {
   EditTextPage({
     Key key,
     this.title,
     this.initValue,
     this.hintText,
-  })
-      : _controller = new TextEditingController(
-          text: initValue,
-        );
+  });
 
   final String title;
   final String initValue;
   final String hintText;
-  final TextEditingController _controller;
+
+  @override
+  _EditTextPageState createState() => new _EditTextPageState();
+}
+
+class _EditTextPageState extends State<EditTextPage> {
+  TextEditingController _controller;
+
+  @override
+  initState() {
+    super.initState();
+
+    _controller = new TextEditingController(
+      text: widget.initValue,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(title),
+        title: new Center(
+          child: new Text(widget.title),
+        ),
       ),
-      body: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: new TextField(
-              controller: _controller,
-              autofocus: true,
-              decoration: new InputDecoration(
-                hintText: hintText,
-              ),
-              onSubmitted: (String newValue) {
-                Navigator.of(context).pop(newValue);
-              },
+      body: new Center(
+        child: new Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: new TextField(
+            controller: _controller,
+            autofocus: true,
+            decoration: new InputDecoration(
+              hintText: widget.hintText,
             ),
+            onSubmitted: (String newValue) {
+              Navigator.of(context).pop(newValue);
+            },
           ),
-        ],
+        ),
       ),
     );
   }
